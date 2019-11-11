31f73e2b95a5dd15c4681e05abe1da03776963f2
Javid Sheikh <javid@krowd9.com>
2019-11-11 13:05:24 +0000

    Initial Commit

---
+++ BTCTicket/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BTCTicket
+//
+//  Created by Javid Sheikh on 11/11/2019.
+//  Copyright © 2019 Quaxo Digital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
